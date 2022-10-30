5bef9a173e2f55c0ef153522ea5f68e7c6c873ee
Echoes_ <lsywangyi626@163.com>
2022-10-30 21:57:17 +0800

    - 接入swagger2 knife4j

--- service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/atguigu/system/controller/SysRoleController.java
@@ -2,11 +2,14 @@ package com.atguigu.system.controller;
 
 import com.atguigu.model.system.SysRole;
 import com.atguigu.system.service.SysRoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "角色管理")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -14,12 +17,14 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation(value = "获取全部角色列表")
     @GetMapping("findAll")
     public List<SysRole> findAll(){
         List<SysRole> list = sysRoleService.list();
         return list;
     }
 
+    @ApiOperation(value = "根据id删除角色")
     @DeleteMapping("remove/{id}")
     public boolean removeRole(@PathVariable Long id){
         boolean isSuccess = sysRoleService.removeById(id);
