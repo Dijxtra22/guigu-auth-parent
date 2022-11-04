a5aaa6fb1ae5b034f7c1e6c643bf4b974f5bc65a
Echoes_ <lsywangyi626@163.com>
2022-11-04 11:31:57 +0800

    - 菜单管理（代码生成）

---
+++ service-system/src/main/java/com/atguigu/system/service/SysMenuService.java
@@ -0,0 +1,16 @@
+package com.atguigu.system.service;
+
+import com.atguigu.model.system.SysMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 菜单表 服务类
+ * </p>
+ *
+ * @author atguigu
+ * @since 2022-11-04
+ */
+public interface SysMenuService extends IService<SysMenu> {
+
+}
