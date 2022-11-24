96c855036b4052e1bf7518072e825a6184fc2d64
Echoes_ <lsywangyi626@163.com>
2022-11-24 14:39:47 +0800

    - 用户授权 - - 添加spring security的AccessDeniedException全局异常处理

--- common/service-util/src/main/java/com/atguigu/system/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/atguigu/system/exception/GlobalExceptionHandler.java
@@ -1,27 +1,43 @@
 package com.atguigu.system.exception;
 
 import com.atguigu.common.result.Result;
+import com.atguigu.common.result.ResultCodeEnum;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public Result error(Exception e){
+    public Result error(Exception e) {
         e.printStackTrace();
         return Result.fail().message("全局异常处理");
     }
 
     @ExceptionHandler(ArithmeticException.class)
-    public Result error(ArithmeticException e){
+    public Result error(ArithmeticException e) {
         e.printStackTrace();
         return Result.fail().message("执行了特定异常处理");
     }
 
     @ExceptionHandler(GuiguException.class)
-    public Result error(GuiguException e){
+    public Result error(GuiguException e) {
         e.printStackTrace();
         return Result.fail().message(e.getMessage()).code(e.getCode());
+    }
+
+    /**
+     * spring security异常
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseBody
+    public Result error(AccessDeniedException e) throws AccessDeniedException {
+        return Result.fail().code(ResultCodeEnum.PERMISSION.getCode()).message("没有当前功能操作权限");
     }
 }
