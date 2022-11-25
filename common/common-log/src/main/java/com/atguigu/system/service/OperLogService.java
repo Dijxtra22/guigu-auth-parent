5e4bee818b2468612313e95138f76a1d9814614b
Echoes_ <lsywangyi626@163.com>
2022-11-25 12:00:39 +0800

    - 登录日志和操作日志的查询接口

--- common/common-log/src/main/java/com/atguigu/system/service/OperLogService.java
+++ common/common-log/src/main/java/com/atguigu/system/service/OperLogService.java
@@ -1,7 +1,14 @@
 package com.atguigu.system.service;
 
 import com.atguigu.model.system.SysOperLog;
+import com.atguigu.model.vo.SysOperLogQueryVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 public interface OperLogService {
     public void saveSysLog(SysOperLog sysOperLog);
+
+    IPage<SysOperLog> selectPage(Page<SysOperLog> pageParam, SysOperLogQueryVo sysOperLogQueryVo);
+
+    SysOperLog getById(Long id);
 }
--- service-system/src/main/java/com/atguigu/system/service/impl/LoginLogServiceImpl.java
+++ service-system/src/main/java/com/atguigu/system/service/impl/LoginLogServiceImpl.java
@@ -1,10 +1,15 @@
 package com.atguigu.system.service.impl;
 
 import com.atguigu.model.system.SysLoginLog;
+import com.atguigu.model.vo.SysLoginLogQueryVo;
 import com.atguigu.system.mapper.LoginLogMapper;
 import com.atguigu.system.service.LoginLogService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class LoginLogServiceImpl implements LoginLogService {
@@ -20,5 +25,30 @@ public class LoginLogServiceImpl implements LoginLogService {
         log.setIpaddr(ipaddr);
         log.setMsg(message);
         loginLogMapper.insert(log);
+    }
+
+    @Override
+    public IPage<SysLoginLog> selectPage(final Page<SysLoginLog> pageParam, final SysLoginLogQueryVo sysLoginLogQueryVo) {
+        String username = sysLoginLogQueryVo.getUsername();
+        String createTimeBegin = sysLoginLogQueryVo.getCreateTimeBegin();
+        String createTimeEnd = sysLoginLogQueryVo.getCreateTimeEnd();
+        QueryWrapper<SysLoginLog> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(username)) {
+            wrapper.like("username", username);
+        }
+        if (!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge("create_time", createTimeBegin);
+        }
+        if (!StringUtils.isEmpty(createTimeEnd)) {
+            wrapper.le("create_time", createTimeEnd);
+        }
+
+        Page<SysLoginLog> sysLoginLogPage = loginLogMapper.selectPage(pageParam, wrapper);
+        return sysLoginLogPage;
+    }
+
+    @Override
+    public SysLoginLog getById(final Long id) {
+        return loginLogMapper.selectById(id);
     }
 }
